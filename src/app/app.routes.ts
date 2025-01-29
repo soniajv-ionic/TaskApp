7866674e95b86af7adf08d27d051f3d4d862356a
soniajv-ionic <soniajv.ionic@gmail.com>
2025-01-29 13:27:47 +0100

    update 29/01/2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'task-splash-screen',
+    redirectTo: 'task-all',//'task-splash-screen',
     pathMatch: 'full',
   },
   {
--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,15 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { IonicStorageModule } from '@ionic/storage-angular';
+
+import { importProvidersFrom } from '@angular/core';//Persistence
+import { IonicStorageModule } from '@ionic/storage-angular';//Persistence
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    importProvidersFrom(IonicStorageModule.forRoot()), //Persistencia de datos
+    importProvidersFrom(IonicStorageModule.forRoot())//Persistence
   ],
 });
